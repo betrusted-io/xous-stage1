088da7e9449ceba3c056212e9b0d2d8e0d5eb77e
Sean Cross <sean@xobs.io>
2020-02-13 16:58:31 +0800

    init: finish preparing everything for kernel
    In theory, everything should be set up for the kernel now.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,20 @@ pub type XousPid = u8;
 const PAGE_SIZE: u32 = 4096;
 const MAX_PROCESS_COUNT: usize = 256;
 
+const FLG_VALID: u32 = 0x1;
+const FLG_X: u32 = 0x4;
+const FLG_W: u32 = 0x4;
+const FLG_R: u32 = 0x2;
+const FLG_U: u32 = 0x10;
+const FLG_A: u32 = 0x40;
+const FLG_D: u32 = 0x80;
+
+use core::panic::PanicInfo;
+#[panic_handler]
+fn handle_panic(_arg: &PanicInfo) -> ! {
+    loop {}
+}
+
 /// Convert a four-letter string into a 32-bit int.
 macro_rules! make_type {
     ($fcc:expr) => {{
@@ -116,24 +130,9 @@ struct ProgramDescription {
     stack_offset: u32,
 }
 
-struct Allocator {
-    next_page_offset: u32,
-    runtime_page_tracker: *mut XousPid,
-    sram_start: *mut u8,
-
-    /// The start of the memory region listing
-    regions_start: *mut u32,
-
-    /// The size of the memory region listing, in bytes
-    regions_size: u32,
-}
-
 extern "C" {
     /// Set the stack pointer to the given address.
     fn set_sp(sp: u32);
-
-    // Enable the MMU with the given translation pointer
-    fn set_satp(satp: u32);
 
 // fn start_kernel(entrypoint: u32, args: u32, )
 }
@@ -146,7 +145,7 @@ impl ProgramDescription {
     /// for this reason.
     pub fn load(
         &self,
-        allocator: &mut Allocator,
+        allocator: &mut BootConfig,
         load_offset: u32,
         is_kernel: bool,
         processes: &mut SystemServices,
@@ -154,30 +153,42 @@ impl ProgramDescription {
         let initial_pid = if is_kernel { 0 } else { 1 };
         // Not a great algorithm!
         for pid in (initial_pid as usize)..processes.processes.len() {
+            // The upper bit indicates whether the SATP is valid or not.
+            // If this bit is set, then this process is allocated
             if processes.processes[pid].satp & 0x80000000 != 0 {
                 continue;
             }
-            let sp_page = allocator.alloc() as u32;
 
+            // Allocate a page to handle the top-level memory translation
             let satp = allocator.alloc() as u32;
-            unsafe { set_satp((satp >> 10) | 0x80000000 | ((pid as u32 + 1) << 22)) };
+            allocator.change_owner(pid as XousPid, satp);
             processes.processes[pid].satp = satp;
+
+            // Turn the satp address into a pointer
             let satp = unsafe { &mut *(satp as *mut PageTable) };
 
-            allocator.map_page(satp, sp_page, self.stack_offset);
+            // Allocate a page for stack
+            let sp_page = allocator.alloc() as u32;
+            allocator.map_page(satp, sp_page, self.stack_offset, FLG_W | FLG_R);
+            allocator.change_owner(pid as XousPid, sp_page);
+
             assert!((self.load_offset & (PAGE_SIZE - 1)) == 0);
             assert!((self.data_offset & (PAGE_SIZE - 1)) == 0);
 
             // Map the process text section into RAM.
+            // Either this is on SPI flash at an aligned address, or it
+            // has been copied into RAM already.  This is why we ignore `self.load_offset`
+            // and use the `load_offset` parameter instead.
             for offset in (0..self.load_size).step_by(PAGE_SIZE as usize) {
-                let page_addr = allocator.alloc();
-                allocator.map_page(satp, load_offset + offset, self.text_offset + offset);
+                allocator.map_page(satp, load_offset + offset, self.text_offset + offset, FLG_W | FLG_R | FLG_X | FLG_U);
+                allocator.change_owner(pid as XousPid, load_offset + offset);
             }
 
             // Map the process data section into RAM.
             for offset in (0..self.data_size).step_by(PAGE_SIZE as usize) {
                 let page_addr = allocator.alloc();
-                allocator.map_page(satp, page_addr as u32, self.data_offset + offset);
+                allocator.map_page(satp, page_addr as u32, self.data_offset + offset, FLG_W | FLG_R | FLG_U);
+                allocator.change_owner(pid as XousPid, load_offset + offset);
             }
             processes.processes[pid].pc = self.entrypoint;
             processes.processes[pid].state = 1;
@@ -185,96 +196,6 @@ impl ProgramDescription {
             return self.entrypoint;
         }
         panic!("no free PID found");
-    }
-}
-
-impl Allocator {
-    /// Zero-alloc a new page, mark it as owned by PID1, and return it.
-    /// Decrement the `next_page_offset` (npo) variable by one page.
-    pub fn alloc(&mut self) -> *mut u32 {
-        self.next_page_offset = self.next_page_offset - PAGE_SIZE as u32;
-        unsafe {
-            // Grab the page address and zero it out
-            let pg = self.sram_start.add(self.next_page_offset as usize);
-            bzero(pg as *mut u32, pg.add(PAGE_SIZE as usize) as *mut u32);
-
-            // Mark this page as in-use by PID1
-            self.runtime_page_tracker
-                .add((self.next_page_offset / PAGE_SIZE) as usize)
-                .write_volatile(1);
-
-            // Return the address
-            pg as *mut u32
-        }
-    }
-
-    pub fn change_owner(
-        &mut self,
-        pid: XousPid,
-        region: u32,
-        regions_base: *mut u32,
-        regions_size: u32,
-    ) {
-        let mut runtime_page_tracker_len = 0;
-        for region_offset in (0..(regions_size / 4) as usize).step_by(3) {
-            let region_start = unsafe { regions_base.add(region_offset + 0).read() };
-            let region_length = unsafe { regions_base.add(region_offset + 1).read() };
-            // let _region_name = cfg.regions_start.add(region_offset + 2).read();
-            if region >= region_start && region < region_start + region_length {
-                unsafe {
-                    self.runtime_page_tracker
-                    .add((runtime_page_tracker_len + ((region - region_start) / PAGE_SIZE)) as usize)
-                    .write_volatile(pid);
-                }
-                return;
-            }
-            runtime_page_tracker_len += region_length * mem::size_of::<XousPid>() as u32 / PAGE_SIZE;
-        }
-    }
-
-    /// Map the given page to the specified process table.  If necessary,
-    /// allocate a new page.
-    ///
-    /// # Panics
-    ///
-    /// * If you try to map a page twice
-    pub fn map_page(&mut self, root: &mut PageTable, phys: u32, virt: u32) {
-        let ppn1 = (phys >> 22) & ((1 << 12) - 1);
-        let ppn0 = (phys >> 12) & ((1 << 10) - 1);
-        let ppo = (phys >> 0) & ((1 << 12) - 1);
-
-        let vpn1 = (virt >> 22) & ((1 << 10) - 1);
-        let vpn0 = (virt >> 12) & ((1 << 10) - 1);
-        let vpo = (virt >> 0) & ((1 << 12) - 1);
-
-        assert!(ppn1 < 4096);
-        assert!(ppn0 < 1024);
-        assert!(ppo < 4096);
-        assert!(vpn1 < 1024);
-        assert!(vpn0 < 1024);
-        assert!(vpo < 4096);
-
-        let ref mut l1_pt = root.entries;
-
-        // Allocate a new level 1 pagetable entry if one doesn't exist.
-        if l1_pt[vpn1 as usize] & 1 == 0 {
-            let new_addr = self.alloc() as u32;
-
-            // Mark this entry as a leaf node (WRX as 0), and indicate
-            // it is a valid page by setting "V".
-            let ppn = new_addr >> 12;
-            l1_pt[vpn1 as usize] = (ppn << 10) | 1;
-        }
-
-        let l0_pt_idx =
-            unsafe { &mut (*(((l1_pt[vpn1 as usize] << 2) & !((1 << 12) - 1)) as *mut PageTable)) };
-        let ref mut l0_pt = l0_pt_idx.entries;
-
-        // Ensure the entry hasn't already been mapped.
-        if l0_pt[vpn0 as usize] & 1 != 0 {
-            panic!("Page already allocated!");
-        }
-        l0_pt[vpn0 as usize] = (ppn1 << 20) | (ppn0 << 10) | 1 | 0xe | 0xd0;
     }
 }
 
@@ -301,12 +222,6 @@ where
     }
 }
 
-use core::panic::PanicInfo;
-#[panic_handler]
-fn handle_panic(_arg: &PanicInfo) -> ! {
-    loop {}
-}
-
 fn read_next_tag(b8: *const u8, byte_offset: &mut usize) -> Result<(u32, u32, u32), ()> {
     let tag_name = u32::from_le(unsafe { (b8 as *mut u32).add(*byte_offset / 4).read() }) as u32;
     *byte_offset += 4;
@@ -321,12 +236,15 @@ fn read_initial_config(b8: *const u8, cfg: &mut BootConfig) {
     let mut byte_offset = 0;
 
     let (tag_name, _crc, size) =
-        read_next_tag(b8, &mut byte_offset).expect("couldn't read next tag");
-    if tag_name != make_type!("XASZ") || size != 4 {
-        panic!("XASZ wasn't first tag, or was invalid size");
+        read_next_tag(b8, &mut byte_offset).expect("couldn't read initial tag");
+    if tag_name != make_type!("XArg") || size != 20 {
+        panic!("XArg wasn't first tag, or was invalid size");
     }
     let total_bytes = unsafe { (b8 as *const usize).add(byte_offset / 4).read() } * 4;
-    byte_offset += 4;
+    cfg.sram_start = unsafe { (b8.add(byte_offset + 8) as *mut u32).read() } as *mut u32;
+    cfg.sram_size = unsafe { (b8.add(byte_offset + 12) as *mut u32).read() };
+    byte_offset += size as usize;
+
     cfg.args_size = total_bytes as u32;
     let mut kernel_seen = false;
     let mut init_seen = false;
@@ -337,8 +255,6 @@ fn read_initial_config(b8: *const u8, cfg: &mut BootConfig) {
         if tag_name == make_type!("MBLK") {
             cfg.regions_start = unsafe { b8.add(byte_offset) as *mut u32 };
             cfg.regions_size = size;
-            cfg.sram_start = unsafe { (b8.add(byte_offset + 0) as *mut u32).read() } as *mut u32;
-            cfg.sram_size = unsafe { (b8.add(byte_offset + 4) as *mut u32).read() };
         } else if tag_name == make_type!("Bflg") {
             let boot_flags = unsafe { (b8.add(byte_offset) as *mut u32).read() };
             if boot_flags & 1 != 0 {
@@ -376,18 +292,17 @@ fn copy_processes(cfg: &mut BootConfig, args: *const u8) {
         let (tag_name, _crc, size) =
             read_next_tag(args, &mut byte_offset).expect("couldn't read next tag");
         if tag_name == make_type!("XKRN") || tag_name == make_type!("Init") {
-            let load_offset = unsafe { (args.add(byte_offset) as *mut u32).add(0).read() };
-            let load_size = unsafe { (args.add(byte_offset) as *mut u32).add(1).read() };
+            let prog = unsafe { &*(args.add(byte_offset) as *const ProgramDescription) };
 
             // Round it off to a page boundary
-            let load_size_rounded = (load_size + 4096 - 1) & !(4096 - 1);
+            let load_size_rounded = (prog.load_size + PAGE_SIZE - 1) & !(PAGE_SIZE - 1);
             cfg.init_size += load_size_rounded;
-            top = unsafe { top.sub(load_size_rounded as usize) };
-            let base = unsafe { cfg.base_addr.add(load_offset as usize / 4) };
-            unsafe { memcpy(top, base, load_size as usize) };
             unsafe {
+                top = top.sub(load_size_rounded as usize);
+                let base = cfg.base_addr.add(prog.load_offset as usize / 4);
+                memcpy(top, base, prog.load_size as usize);
                 bzero(
-                    top.add(load_size as usize / 4),
+                    top.add(prog.load_size as usize / 4),
                     top.add(load_size_rounded as usize / 4),
                 )
             };
@@ -406,8 +321,8 @@ fn copy_processes(cfg: &mut BootConfig, args: *const u8) {
 /// Stage 1 Bootloader
 /// This makes the program self-sufficient by setting up memory page assignment
 /// and copying the arguments to RAM
-#[export_name = "stage1"]
-pub unsafe extern "C" fn early_init(arg_buffer: *const u8, signature: u32) -> ! {
+#[export_name = "rust_entry"]
+pub unsafe extern "C" fn rust_entry(arg_buffer: *const u8, signature: u32) -> ! {
     stage1(arg_buffer, signature);
 }
 
@@ -418,6 +333,106 @@ impl BootConfig {
                 .add((self.sram_size - self.init_size) as usize / 4)
         }
     }
+
+    /// Zero-alloc a new page, mark it as owned by PID1, and return it.
+    /// Decrement the `next_page_offset` (npo) variable by one page.
+    pub fn alloc(&mut self) -> *mut u32 {
+        let pg = self.get_top();
+        self.init_size += PAGE_SIZE;
+        unsafe {
+            // Grab the page address and zero it out
+            bzero(pg as *mut u32, pg.add(PAGE_SIZE as usize) as *mut u32);
+
+            // Mark this page as in-use by PID1
+            self.runtime_page_tracker
+                .add((self.init_size / PAGE_SIZE) as usize)
+                .write_volatile(1);
+        };
+
+            // Return the address
+            pg as *mut u32
+    }
+
+    pub fn change_owner(
+        &mut self,
+        pid: XousPid,
+        region: u32,
+    ) {
+        // First, check to see if the region is in RAM,
+        if region > self.sram_start as u32 && region < self.sram_start as u32 + self.sram_size {
+            unsafe {
+                self.runtime_page_tracker
+                .add(((region - self.sram_start as u32) / PAGE_SIZE) as usize)
+                .write_volatile(pid);
+            }
+            return;
+        }
+
+        // The region isn't in RAM, so check the other memory regions.
+        let mut runtime_page_tracker_len = self.sram_size * mem::size_of::<XousPid>() as u32 / PAGE_SIZE;
+
+        for region_offset in (0..(self.regions_size / 4) as usize).step_by(3) {
+            let region_start = unsafe { self.regions_start.add(region_offset + 0).read() };
+            let region_length = unsafe { self.regions_start.add(region_offset + 1).read() };
+            // let _region_name = cfg.regions_start.add(region_offset + 2).read();
+            if region >= region_start && region < region_start + region_length {
+                unsafe {
+                    self.runtime_page_tracker
+                    .add((runtime_page_tracker_len + ((region - region_start) / PAGE_SIZE)) as usize)
+                    .write_volatile(pid);
+                }
+                return;
+            }
+            runtime_page_tracker_len += region_length * mem::size_of::<XousPid>() as u32 / PAGE_SIZE;
+        }
+        panic!("Tried to change region {:08x} that isn't in the memory region!", region);
+    }
+
+    /// Map the given page to the specified process table.  If necessary,
+    /// allocate a new page.
+    ///
+    /// # Panics
+    ///
+    /// * If you try to map a page twice
+    pub fn map_page(&mut self, root: &mut PageTable, phys: u32, virt: u32, flags: u32) {
+        let ppn1 = (phys >> 22) & ((1 << 12) - 1);
+        let ppn0 = (phys >> 12) & ((1 << 10) - 1);
+        let ppo = (phys >> 0) & ((1 << 12) - 1);
+
+        let vpn1 = (virt >> 22) & ((1 << 10) - 1);
+        let vpn0 = (virt >> 12) & ((1 << 10) - 1);
+        let vpo = (virt >> 0) & ((1 << 12) - 1);
+
+        assert!(ppn1 < 4096);
+        assert!(ppn0 < 1024);
+        assert!(ppo < 4096);
+        assert!(vpn1 < 1024);
+        assert!(vpn0 < 1024);
+        assert!(vpo < 4096);
+
+        let ref mut l1_pt = root.entries;
+
+        // Allocate a new level 1 pagetable entry if one doesn't exist.
+        if l1_pt[vpn1 as usize] & 1 == 0 {
+            let new_addr = self.alloc() as u32;
+
+            // Mark this entry as a leaf node (WRX as 0), and indicate
+            // it is a valid page by setting "V".
+            let ppn = new_addr >> 12;
+            l1_pt[vpn1 as usize] = (ppn << 10) | 1;
+        }
+
+        let l0_pt_idx =
+            unsafe { &mut (*(((l1_pt[vpn1 as usize] << 2) & !((1 << 12) - 1)) as *mut PageTable)) };
+        let ref mut l0_pt = l0_pt_idx.entries;
+
+        // Ensure the entry hasn't already been mapped.
+        if l0_pt[vpn0 as usize] & 1 != 0 {
+            panic!("Page already allocated!");
+        }
+        l0_pt[vpn0 as usize] = (ppn1 << 20) | (ppn0 << 10) | flags | FLG_VALID | FLG_D | FLG_A;
+        // 1 | 0xe | 0xd0;
+    }
 }
 
 /// Allocate and initialize memory regions.
@@ -425,7 +440,8 @@ impl BootConfig {
 fn allocate_regions(cfg: &mut BootConfig) -> (*mut XousPid, u32) {
     let top = cfg.get_top();
     // Number of individual pages in the system
-    let mut runtime_page_tracker_len = 0;
+    let mut runtime_page_tracker_len = cfg.sram_start as u32 * mem::size_of::<XousPid>() as u32 / PAGE_SIZE;
+
     for region_offset in (0..(cfg.regions_size / 4) as usize).step_by(3) {
         // let _region_start = cfg.regions_start.add(region_offset + 0).read();
         let region_length = unsafe { cfg.regions_start.add(region_offset + 1).read() };
@@ -481,10 +497,13 @@ fn copy_args(cfg: &mut BootConfig, arg_buffer: *const u8) {
             cfg.args_size as usize,
         )
     };
+    // TODO: Patch up regions_start
     cfg.args_start = runtime_arg_buffer as *const u8;
 }
 
 fn stage1(arg_buffer: *const u8, _signature: u32) -> ! {
+    // Store the initial boot config on the stack.  We don't know
+    // where in heap this memory will go.
     let mut cfg = BootConfig {
         no_copy: false,
         base_addr: arg_buffer as *const u32,
@@ -501,6 +520,12 @@ fn stage1(arg_buffer: *const u8, _signature: u32) -> ! {
         process_offset: 0,
     };
     read_initial_config(arg_buffer, &mut cfg);
+
+    // Allocate a stack pointer.
+    // Allocate us a stack pointer directly below the memory page tracker,
+    // and enable it.
+    let sp = unsafe { cfg.get_top().sub(1) };
+    cfg.init_size += PAGE_SIZE;
 
     // The first region is defined as being "main RAM", which will be used
     // to keep track of allocations.
@@ -519,13 +544,6 @@ fn stage1(arg_buffer: *const u8, _signature: u32) -> ! {
 
     // All further allocations must be page-aligned.
     cfg.init_size = (cfg.init_size + 4096 - 1) & !(4096 - 1);
-
-    // Allocate a stack pointer
-    // Up until now we've been using the stack pointer from the bootloader.
-    // Allocate us a stack pointer directly below the memory page tracker,
-    // and enable it.
-    let sp = unsafe { cfg.get_top().sub(1) };
-    cfg.init_size += PAGE_SIZE;
 
     cfg.process_offset = cfg.init_size;
 
@@ -552,19 +570,13 @@ fn stage1(arg_buffer: *const u8, _signature: u32) -> ! {
 /// Stage 2 bootloader
 /// This sets up the MMU and loads both PID1 and the kernel into RAM.
 fn stage2(cfg: &mut BootConfig) -> ! {
-    let mut allocator = Allocator {
-        runtime_page_tracker: cfg.runtime_page_tracker,
-        sram_start: cfg.sram_start as *mut u8,
-        next_page_offset: cfg.sram_size - cfg.init_size,
-        regions_size: cfg.regions_size,
-        regions_start: cfg.regions_start,
-    };
     let mut system_services = unsafe { &mut (*cfg.system_services) };
 
-    // Loop through the kernel args and copy the kernel and PID1
+    // Loop through the kernel args and copy the kernel and Init
     let mut args_offset = 0;
 
-    let mut process_offset = cfg.sram_size - cfg.process_offset;
+    // This is the offset in RAM where programs are loaded from.
+    let mut process_offset = cfg.sram_start as u32 + cfg.sram_size - cfg.process_offset;
 
     // Go through all Init processes and the kernel, setting up their
     // page tables and mapping memory to them.
@@ -574,12 +586,12 @@ fn stage2(cfg: &mut BootConfig) -> ! {
 
         if tag_name == make_type!("Init") {
             let init = unsafe { &*(cfg.args_start.add(args_offset) as *const ProgramDescription) };
-            init.load(&mut allocator, process_offset, false, &mut system_services);
+            init.load(cfg, process_offset, false, &mut system_services);
             let load_size_rounded = (init.load_size + 4096 - 1) & !(4096 - 1);
             process_offset -= load_size_rounded;
         } else if tag_name == make_type!("XKRN") {
             let xkrn = unsafe { &*(cfg.args_start.add(args_offset) as *const ProgramDescription) };
-            xkrn.load(&mut allocator, process_offset, true, &mut system_services);
+            xkrn.load(cfg, process_offset, true, &mut system_services);
             let load_size_rounded = (xkrn.load_size + 4096 - 1) & !(4096 - 1);
             process_offset -= load_size_rounded;
         }
